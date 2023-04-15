634e9237af0556dee9d61c8069711355c42a00b7
Lucas Anderson de Souza Marques <lucasandersonmarques@gmail.com>
2023-04-15 14:47:02 -0300

    commit de finalização do programa

---
+++ Exercício módulo 7/main.js
@@ -0,0 +1,28 @@
+let form = document.getElementById("verificador");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    
+    const valorUm = parseFloat(document.getElementById("valor-1").value);
+    const valorDois = parseFloat(document.getElementById("valor-2").value);
+    const successMessage = `Perfeito! ${valorDois} é MAIOR que ${valorUm}!`
+    const errorMessage = `Cuidado! ${valorDois} é MENOR que ${valorUm}!`
+
+    function validarNumero (valorUm, valorDois){
+        return valorDois > valorUm;
+    }
+
+    formEValido = validarNumero(valorUm, valorDois);
+
+    if(formEValido) {
+        const containerSuccessMessage = document.querySelector(".success-message");
+        containerSuccessMessage.innerHTML = successMessage;
+        containerSuccessMessage.style.display = "block";
+
+    }
+    else {
+        const containerErrorMessage = document.querySelector(".error-message");
+        containerErrorMessage.innerHTML = errorMessage;
+        containerErrorMessage.style.display = "block";
+    }
+})
